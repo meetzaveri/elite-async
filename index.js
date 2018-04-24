ceba54b0c01349bb7818b85cebb5afd27bc70d8a
Meet Zaveri <meet.zaveri@Simforms-161.local>
2018-04-24 16:08:40 +0530

    Refactoring async.auto()

--- index.js
+++ index.js
@@ -139,27 +139,33 @@ const auto = require('./lib/auto')
 
 auto({
   get_data: function(callback) {
-      console.log('in get_data');
-      // async code to get some data
+    console.log('in get_data');
+    // async code to get some data
+    
+    setTimeout(function() {
       callback(null, 'data', 'converted to array');
+    }, 3000);
   },
   make_folder: function(callback) {
-      console.log('in make_folder');
-      // async code to create a directory to store a file in
-      // this is run at the same time as getting the data
+    console.log('in make_folder');
+    // async code to create a directory to store a file in
+    // this is run at the same time as getting the data
+    
+    setTimeout(function() {
       callback(null, 'folder');
+    }, 2000);
   },
   write_file: ['get_data', 'make_folder', function(results, callback) {
-      console.log('in write_file', JSON.stringify(results));
-      // once there is some data and the directory exists,
-      // write the data to a file in the directory
-      callback(null, 'filename');
-  }],
-  go_file: ['write_file', function(results, callback) {
-    console.log('in go_file', JSON.stringify(results));
+    console.log('in write_file', JSON.stringify(results));
     // once there is some data and the directory exists,
     // write the data to a file in the directory
-    callback(null, 'go_file','Done');
+    callback(null, 'filename');
+  }],
+  make_file: ['write_file', function(results, callback) {
+    console.log('in make_file', JSON.stringify(results));
+    // once there is some data and the directory exists,
+    // write the data to a file in the directory
+    callback(null, {'file':results, 'email':'user@example.com'});
   }]
 }, function(err, results) {
   console.log('err = ', err);
