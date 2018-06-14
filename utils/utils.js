14256db378157f86b76eb689c1019a82e1a17268
Meet Zaveri <meethpatel96@gmail.com>
2018-06-14 17:41:10 +0530

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,6 @@
+// a utility to check if given value is object or not
 function isObject(o) {
   return o !== null && typeof o === 'object' && Array.isArray(o) === false;
 }
 
-// a utility to check if given value is object or not
- module.exports = {isObject:isObject}
+module.exports = {isObject:isObject}
