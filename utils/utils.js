4f930235da23ad697368862b00b2d1c7791b351f
Meet Zaveri <meethpatel96@gmail.com>
2018-07-14 09:03:04 +0530

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -3,4 +3,13 @@ function isObject(o) {
   return o !== null && typeof o === 'object' && Array.isArray(o) === false;
 }
 
-module.exports = {isObject:isObject}
+const isSorted = arr => {
+  let direction = -(arr[0] - arr[1]);
+  for (let [i, val] of arr.entries()) {
+    direction = !direction ? -(arr[i - 1] - arr[i]) : direction;
+    if (i === arr.length - 1) return !direction ? 0 : direction;
+    else if ((val - arr[i + 1]) * direction > 0) return 0;
+  }
+};
+
+module.exports = {isObject,isSorted}
